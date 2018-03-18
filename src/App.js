5b746a1052106d7f8c7cbd8f13465bc93f0ccdfb
Simon Ingeson <simon.ingeson@gmail.com>
2018-03-17 22:18:16 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from 'react';
-import { css } from 'emotion';
 import { hot } from 'react-hot-loader';
+import { css } from 'emotion';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+import AboutPage from './components/AboutPage';
+import HomePage from './components/HomePage';
 
 const styles = {
-  container: css({
-    padding: '1.5rem',
+  nav: css({
+    display: 'flex',
+    backgroundColor: '#f8f8f8',
+    borderBottom: '1px solid #f0f0f0',
+    padding: 10,
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+  }),
+
+  link: css({
+    color: '#333',
+
+    ':hover': {
+      color: 'black',
+    },
+
+    ':not(:first-child)': {
+      marginLeft: 10,
+    },
+
+    '&.active': {
+      color: '#666',
+      textDecoration: 'none',
+    },
+  }),
+
+  content: css({
+    marginTop: 'calc(21px + 1.5rem)',
+    padding: 10,
   }),
 };
 
-const App = () => <div className={styles.container}>Hello, World!</div>;
+const App = () => (
+  <Router>
+    <div>
+      <nav className={styles.nav}>
+        <NavLink className={styles.link} exact to="/">
+          Home
+        </NavLink>
+        <NavLink className={styles.link} to="/about">
+          About
+        </NavLink>
+      </nav>
+
+      <div className={styles.content}>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+      </div>
+    </div>
+  </Router>
+);
 
 export default hot(module)(App);
